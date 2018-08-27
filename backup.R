1e7fccbc201f368c67236bc5c18026421b016a60
HalforcNull <yaorunan@gmail.com>
2018-08-27 08:40:12 -0500

    preprocess kallisto result for PC3

--- backup.R
+++ backup.R
@@ -4,6 +4,7 @@ biocLite("biomaRt")
 library(biomaRt)
 library(dplyr)
 
+# tophat+cufflink solution
 
 ensembl = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", GRCh=37)
 
@@ -22,7 +23,7 @@ rawresult<- dplyr::inner_join(rawresult_1, hgnc_swissprot, by = 'hgnc_symbol')
 
 PC3M<-rawresult %>%
   select(one_of(c('ensembl_gene_id', 'q7_0', 'q8_0', 'q9_0', 'q10_0', 'q11_0', 'q12_0') ))
-colnames(PC3M) <- c('ensembl_gene_id', 'PC3MNT1', 'PC3MNT2', 'PC3MNT3', 'PC3MTR1', 'PC3MTR2', 'PC3MTR3')
+colnames(PC3M) <- c('ensembl_gene_id', 'PC3M_NT1', 'PC3M_NT2', 'PC3M_NT3', 'PC3M_TR1', 'PC3M_TR2', 'PC3M_TR3')
 
 
 PC3<-rawresult %>%
@@ -33,5 +34,32 @@ colnames(PC3) <- c('ensembl_gene_id', 'PC3_NT1', 'PC3_NT2', 'PC3_NT3', 'PC3_TR1'
 
 write.csv(PC3M, file="pc3m.csv", row.names = FALSE)
 write.csv(PC3, file="pc3.csv", row.names = FALSE)
+
+
+
+gene.count <- read.table("Yale Data Result/genes.count_table", header = TRUE)
+gene.attr <- read.table("Yale Data Result/genes.attr_table", header = TRUE)
+
+gene.tracking.name <- gene.attr %>% select(one_of(c("tracking_id", "gene_short_name")))
+
+
+rawresult_1 <- dplyr::left_join(gene.count, gene.tracking.name, by="tracking_id") 
+names(rawresult_1)[8] <- 'hgnc_symbol'
+rawresult<- dplyr::inner_join(rawresult_1, hgnc_swissprot, by = 'hgnc_symbol')
+
+MCF7<-rawresult %>%
+  select(one_of(c('ensembl_gene_id', 'q1_0', 'q2_0', 'q3_0', 'q4_0', 'q5_0', 'q6_0') ))
+colnames(MCF7) <- c('ensembl_gene_id', 'MCF7_NT1', 'MCF7_NT2', 'MCF7_NT3', 'MCF7_TR1', 'MCF7_TR2', 'MCF7_TR3')
+
+write.csv(MCF7, file="MCF7.csv", row.names = FALSE)
+
+
+# kallisto solution
+PC3NT1 <- read.table("VerifyPC3/kallistoResult2_NT1/abundance.tsv", header = TRUE)
+PC3NT2 <- read.table("VerifyPC3/kallistoResult2_NT2/abundance.tsv", header = TRUE)
+PC3NT3 <- read.table("VerifyPC3/kallistoResult2_NT3/abundance.tsv", header = TRUE)
+PC3TR1 <- read.table("VerifyPC3/kallistoResult2_TR1/abundance.tsv", header = TRUE)
+PC3TR2 <- read.table("VerifyPC3/kallistoResult2_TR2/abundance.tsv", header = TRUE)
+PC3TR3 <- read.table("VerifyPC3/kallistoResult2_TR3/abundance.tsv", header = TRUE)
 
 
