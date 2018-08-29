5ecbb7ef2d26b9e5567cebacee30b0a8e1c154af
HalforcNull <yaorunan@gmail.com>
2018-08-29 16:29:18 -0500

    more test on user navigated UI

--- cuffnormDataPreprocess/server.R
+++ cuffnormDataPreprocess/server.R
@@ -1,5 +1,4 @@
 library(shiny)
-library(shinyDND)
 
 #upload file max size = 300 MB
 options(shiny.maxRequestSize=300*1024^2) 
@@ -9,30 +8,46 @@ options(shiny.maxRequestSize=300*1024^2)
 #
 shinyServer(function(input, output) {
 
-  observeEvent(input$fCount,{
-    if(! "SampleList" %in% colnames(input))
-    {
-      req(input$fCount)
-      tryCatch(
-        { 
-          df <- read.table(input$fCount$datapath)
-          output$SampleList <- renderTable( head(df) )
-        },
-        error = function(e) {
-          # return a safeError if a parsing error occurs
-          stop(safeError(e))
-        }
-      )
-      
-      output$stage <- renderText( 'group samples' )
-      outputOptions(output, "stage", suspendWhenHidden = FALSE)
-      
-      
-    }
+  # observeEvent(input$fCount,{
+  #   if(! "SampleList" %in% colnames(input))
+  #   {
+  #     req(input$fCount)
+  #     tryCatch(
+  #       { 
+  #         df <- read.table(input$fCount$datapath)
+  #         output$SampleList <- renderTable( head(df) )
+  #       },
+  #       error = function(e) {
+  #         # return a safeError if a parsing error occurs
+  #         stop(safeError(e))
+  #       }
+  #     )
+  #     
+  #     output$stage <- renderText( 'group samples' )
+  #     outputOptions(output, "stage", suspendWhenHidden = FALSE)
+  #     
+  #     
+  #   }
+  # })
+  # 
+  # observeEvent(input$div2,{
+  #   output$foo = renderText(
+  #     paste("The dropUI element currently contains:", input$div2))
+  # })
+  
+  STATE.UserHaveReadCount = FALSE
+  observeEvent(input$userHaveReadCount, {
+    STATE.UserHaveReadCount = TRUE
+    output$MessageStep1 <- renderText('Great! Do you need rename your columns?')
+    output$showPanel2<- reactive(STATE.UserHaveReadCount)
+    outputOptions(output, "showPanel2", suspendWhenHidden = FALSE)
   })
   
-  observeEvent(input$div2,{
-    output$foo = renderText(
-      paste("The dropUI element currently contains:", input$div2))
+  observeEvent(input$userDoNotHaveReadCount, {
+    STATE.UserHaveReadCount = FALSE
+    output$MessageStep1 <- renderText('OK. We need get the read count first.')
+    output$showPanel2<- reactive(STATE.UserHaveReadCount)
+    outputOptions(output, "showPanel2", suspendWhenHidden = FALSE)
   })
+  
 })
--- cuffnormDataPreprocess/ui.R
+++ cuffnormDataPreprocess/ui.R
@@ -1,25 +1,40 @@
 library(shiny)
-library(shinyDND)
 
 
 shinyUI(  
   fluidPage(
-    h1("Upload File"),
-    fileInput("fCount", label = "Count Table"), 
-    fileInput("fAttr", label = "Attr Table"), 
-    conditionalPanel(condition="true",
-                     fluidRow(
-                        column( 1, h2("Samples"),
-                                dragSetUI("div1", textval = c("a","b"))
-                        ),
-                        column( 2,
-                                textInput("textCtrl1", label=NULL, value = "Control 1"),
-                                dropUI("ctrl1"),
-                                textInput("textCtrl2", label=NULL, value = "Control 2"),
-                                dropUI("ctrl2")
-                        )
-                     )
-    )
+    # h1("Upload File"),
+    # fileInput("fCount", label = "Count Table"), 
+    # fileInput("fAttr", label = "Attr Table"), 
+    # conditionalPanel(condition="true",
+    #                  fluidRow(
+    #                     column( 1, h2("Samples"),
+    #                             dragSetUI("div1", textval = c("a","b"))
+    #                     ),
+    #                     column( 2,
+    #                             textInput("textCtrl1", label=NULL, value = "Control 1"),
+    #                             dropUI("ctrl1"),
+    #                             textInput("textCtrl2", label=NULL, value = "Control 2"),
+    #                             dropUI("ctrl2")
+    #                     )
+    #                  )
+    # )
+    # 
+    
+    
+      column(3, h2("Step 1"),
+             p("Do you have read count data ?"),
+             actionButton("userHaveReadCount", "Yes"),
+             actionButton("userDoNotHaveReadCount", "No"),
+             textOutput("MessageStep1")
+             ),
+      
+      # Panel 2
+      conditionalPanel(condition="output.showPanel2 == true",
+                       column(3, h2("Go To Idep") ))
+      
+      
+    
     
   )
 )
